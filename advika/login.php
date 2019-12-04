3d215de21e09dfb3aaca261816c9904427e268d5
sri132 <58459521+sri132@users.noreply.github.com>
2019-12-04 07:13:09 +0530

    Add files via upload

---
+++ advika/login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title> Advika_2020 | Log-in </title>
+  <meta charset="utf-8">
+  <meta name="description" content="Advika | Login">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="login.css" />
+ </head>
+ <body>
+     <!---------------------------Navigation part starts-------------------------------->
+     <div class="navbar">
+      <div class="row">
+	  <div class="container-fluid">
+          <input type="checkbox" id='toggle' class="hidden">
+          <label for='toggle' class='navbtn'>
+              <i></i>
+              <i></i>
+              <i></i>
+          </label>
+        <div class="responsive">
+         <ul>
+	      <li><a href="#"> Home </a></li>
+		  <li><a href=""> About Us </a></li>
+		  <li><a href=""> Guidelines </a></li>
+		  <li><a href=""> Events </a></li>
+          <li><a href=""> Brochure </a></li>
+          <li><a href=""> Contact Us </a></li>
+	      <li><a class="disable" href=""> Log-in </a></li>
+	      <li><a href=""> Register </a></li>
+		 </ul>
+        </div>
+     </div>
+	 </div>
+   </div>
+	<!--------------------------- Navigation part ends --------------------------------->
+	<!--------------------------- Login part starts --------------------------------->
+  <div class="login" >
+      <div class="heading">
+          <h1>Log-in</h1>
+      </div>
+      <form action="#" method="post" >
+          <input name="username" type="email/number" placeholder="Email/Mobile" ></br>
+        <input name="password" type="password" placeholder="Password" ></br>
+        <button type="submit" > Sign-in </button></br>
+      </form>
+  </div>
+    <!--------------------------- Login part ends --------------------------------->
+ </body>
+</html>
